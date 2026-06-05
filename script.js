199f30fb75357d759a274a54992a143808248e6b
Eduard Eminov <eduard.eminov@lamoda.ru>
2026-06-06 02:17:18 +0300

    created site

---
+++ script.js
@@ -0,0 +1,7 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add('visible');
+  });
+}, { threshold: 0.12 });
+
+document.querySelectorAll('.reveal, .job').forEach(el => observer.observe(el));
